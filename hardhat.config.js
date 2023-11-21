0f3f7211c5d6568b876329c859dd60125e3dbf09
Alan Peña <alanpena@Alans-MacBook-Air.local>
2023-11-21 07:50:59 -0600

    feat: hardhat config init

---
+++ hardhat.config.js
@@ -0,0 +1,14 @@
+/** @type import('hardhat/config').HardhatUserConfig */
+require('dotenv').config();
+require("@nomiclabs/hardhat-ethers");
+const { API_URL, PRIVATE_KEY } = process.env;
+module.exports = {
+  solidity: "0.8.9",
+  defaultNetWork: 'sepolia',
+  networks: {
+    sepolia: {
+      url: API_URL || '',
+      accounts: [`0x${PRIVATE_KEY}`]
+    }
+  }
+};
